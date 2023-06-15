e1c9b1352947bd46e1558a265c2a0783c0824dc1
bryanmardono <102572030+bryanmardono@users.noreply.github.com>
2023-06-15 10:34:03 +0000

    Thu, Jun 15, 2023, 6:34 PM +08:00

---
+++ population/population.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int start_pop = get_int("Starting population: ");
+
+    while(start_pop < 9)
+    {
+        int start_pop = get_int("Starting population: ");
+    }
+
+    // TODO: Prompt for end size
+
+    // TODO: Calculate number of years until we reach threshold
+
+    // TODO: Print number of years
+}
